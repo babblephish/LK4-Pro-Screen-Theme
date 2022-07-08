6da72000fdf48d600182e2431fa04fcdc786c10b
babblephish <83515345+babblephish@users.noreply.github.com>
2022-07-08 16:53:14 -0700

    Fix PID temp for PID tune
    Change min/max range for keyboard to 5/275 for PID tune

--- BuildRelease.bat
+++ BuildRelease.bat
@@ -10,4 +10,3 @@ copy DWIN_SET\*.* release\DWIN_SET
 rem rotate images
 for %%f in (release\DWIN_SET\*.bmp) do magick.exe convert -rotate -90 %%f %%f
 
-xcopy D:\Users\PC\Documents\GitHub\DWIN_LK4Pro\release\DWIN_SET L:\DWIN_SET\ /y
